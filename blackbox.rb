21422157ccf31e9ed383b945d60665325d027b73
jacoelho <jose.coelho@linkedcare.com>
2015-02-02 10:22:21 +0000

    blackbox recipe

---
+++ blackbox.rb
@@ -0,0 +1,15 @@
+class Blackbox < Formula
+  homepage 'https://github.com/StackExchange/blackbox'
+  head 'https://github.com/StackExchange/blackbox.git', :tag => 'production'
+
+  depends_on 'gpg'
+  depends_on 'gpg-agent' => :optional
+
+  def install
+    prefix.install 'bin'
+  end
+
+  test do
+    system '#{bin}/blackbox_cat'
+  end
+end
