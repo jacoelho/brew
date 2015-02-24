db9593f7db8410d5b19c6fdebb3cacf87cffe8f7
jacoelho <jose.coelho@linkedcare.com>
2015-02-24 12:42:23 +0000

    crane formula

---
+++ crane.rb
@@ -0,0 +1,19 @@
+class Crane < Formula
+  homepage 'https://github.com/michaelsauter/crane'
+  url 'https://github.com/michaelsauter/crane.git', :tag => 'v1.1.1'
+  
+  depends_on 'go' => :build
+
+  def install
+    ENV["GOBIN"] = bin
+    ENV["GOPATH"] = buildpath
+    ENV["GOHOME"] = buildpath
+    system "go", "get"
+    system "go", "build", "main.go"
+    bin.install "main" => "crane" 
+  end
+
+  test do
+    system "#{bin}/crane", "version"
+  end
+end
